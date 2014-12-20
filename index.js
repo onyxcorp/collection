590fb0eb92ab9e363f4b2bcd10c8cb172b1a3084
Fabio <fabiozaffani@gmail.com>
2014-12-20 02:20:14 -0200

    Added extend from backbone and update package.json
    The extend method from backbone was created as a method of collection
    constructor.
    
    The package.json was updated to create a dependency on the model module

--- index.js
+++ index.js
@@ -440,6 +440,45 @@ lodash.collections.forEach(attributeMethods, function(method) {
   };
 });
 
-Collection.extend = extend;
+// same extend function used by backbone
+Collection.extend = function(protoProps, staticProps) {
+    var parent,
+        child,
+        Surrogate;
+
+    parent = this;
+
+    // The constructor function for the new subclass is either defined by you
+    // (the "constructor" property in your `extend` definition), or defaulted
+    // by us to simply call the parent's constructor.
+    if (protoProps && _.has(protoProps, 'constructor')) {
+        child = protoProps.constructor;
+    } else {
+        child = function () {
+            return parent.apply(this, arguments);
+        };
+    }
+
+    // Add static properties to the constructor function, if supplied.
+    _.assign(child, parent, staticProps);
+
+    // Set the prototype chain to inherit from `parent`, without calling
+    // `parent`'s constructor function.
+    Surrogate = function () {
+        this.constructor = child;
+    };
+    Surrogate.prototype = parent.prototype;
+    child.prototype = new Surrogate;
+
+    // Add prototype properties (instance properties) to the subclass,
+    // if supplied.
+    if (protoProps) _.assign(child.prototype, protoProps);
+
+    // Set a convenience property in case the parent's prototype is needed
+    // later.
+    child.__super__ = parent.prototype;
+
+    return child;
+};
 
 module.exports = Collection;
