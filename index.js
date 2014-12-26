0ca0b9a4876af71bbc42f04c413f67778ccb9e86
Fabio <fabiozaffani@gmail.com>
2014-12-26 12:37:12 -0200

    Fix and improvements
    - Added a method (modelUpdate) to be called by the model in the
    collection to update the indexes in _byId when the model changes
    - fix the problem that the comparator !== null or === null was causing
    (undefined value was getting by ok causing the _byId to index
    collections with undefined id), still need to find a better way to
    verify if the variable is valid (maybe just using if (something)
    instead, need test)
    - Removed some unnecessary variables like add = options.add, by using
    the object one the code looks more readable (you know the origin of the
    variable inside the method)

--- index.js
+++ index.js
@@ -57,15 +57,15 @@ lodash.objects.assign(Collection.prototype, {
     initialize: function () {},
 
     // The JSON representation of a Collection is an array of the
-    // models' attributes.
+    // models' attributes
     toJSON: function () {
         return this.map( function (model) {
             return model.toJSON();
         });
     },
 
-    // The JSON representation of a Collection as an object representation
-    // with the index being the model key
+    // The JSON representation of a Collection as an object with the index key
+    // being the model key
     toObjectJSON: function () {
         var modelsAttributes,
             modelsData,
@@ -94,9 +94,133 @@ lodash.objects.assign(Collection.prototype, {
 
     // Add a model, or list of models to the set.
     add: function (models, options) {
-        var defaultOptions;
-        defaultOptions = {add: true, remove: false};
-        return this.set(models, lodash.objects.assign({merge: false}, options, defaultOptions));
+        return this.set(models, lodash.objects.assign({merge: false}, options, {add: true, remove: false}));
+    },
+
+    // Update a collection by `set`-ing a new list of models, adding new ones,
+    // removing models that are no longer present, and merging models that
+    // already exist in the collection, as necessary. Similar to **Model#set**,
+    // the core operation for updating the data contained by the collection.
+    set: function (models, options) {
+        var singular,
+            id,
+            model,
+            attrs,
+            existing,
+            sort,
+            sortable,
+            sortAttr,
+            toAdd,
+            toRemove,
+            modelMap,
+            order,
+            orderChanged,
+            i;
+
+        options = lodash.objects.defaults({}, options, {add: true, remove: true, merge: true});
+
+        // check if is a single model collection
+        singular = !lodash.objects.isArray(models);
+
+        models = singular ? (models ? [models] : []) : models.slice();
+        if (options.at < 0) {
+            options.at += this.length + 1;
+        }
+        sortable = this.comparator && (options.at == null) && options.sort !== false;
+        sortAttr = lodash.objects.isString(this.comparator) ? this.comparator : null;
+
+        // initial setup of arrays and objects
+        toAdd = [];
+        toRemove = [];
+        modelMap = {};
+        orderChanged = false;
+
+        // Turn bare objects into model references, and prevent invalid models
+        // from being added.
+        for (i = 0, length = models.length; i < length; i++) {
+            attrs = models[i];
+
+            // If a duplicate is found, prevent it from being added and
+            // optionally merge it into the existing model.
+            existing = this.get(attrs);
+
+            if (existing) {
+                if (options.remove) {
+                    modelMap[existing.cid] = true;
+                }
+                // if options are set to merge the model and the attrs are different
+                // from the current ones passed, updadte the model data
+                if (options.merge && attrs !== existing) {
+                    attrs = this._isModel(attrs) ? attrs.attributes : attrs;
+                    existing.set(attrs, options);
+                    if (sortable && !sort && existing.hasChanged(sortAttr)) {
+                        sort = true;
+                    }
+                }
+                models[i] = existing;
+
+                // If this is a new, valid model, push it to the `toAdd` list.
+            } else if (options.add) {
+                model = models[i] = this._prepareModel(attrs, options);
+                if (!model) continue;
+                toAdd.push(model);
+                this._addReference(model, options);
+            }
+
+            // Do not add multiple models with the same `id`.
+            model = existing || model;
+            if (!model) continue;
+            id = this.modelId(model.attributes);
+            order = !sortable && options.add && options.remove ? [] : false;
+            if (order && (model.isNew() || !modelMap[id])) {
+                order.push(model);
+                // Check to see if this is actually a new model at this index.
+                orderChanged = orderChanged || !this.models[i] || model.cid !== this.models[i].cid;
+            }
+
+            modelMap[id] = true;
+        }
+
+        // Remove nonexistent models if appropriate.
+        if (options.remove) {
+            for (i = 0, length = this.length; i < length; i++) {
+                if (!modelMap[(model = this.models[i]).cid]) {
+                    toRemove.push(model);
+                }
+            }
+            if (toRemove.length) {
+                this.remove(toRemove, options);
+            }
+        }
+
+        // See if sorting is needed, update `length` and splice in new models.
+        if (toAdd.length || orderChanged) {
+            if (sortable) {
+                sort = true;
+            }
+            this.length += toAdd.length;
+            if (options.at !== null) {
+                for (i = 0, length = toAdd.length; i < length; i++) {
+                    this.models.splice(options.at + i, 0, toAdd[i]);
+                }
+            } else {
+                if (order) {
+                    this.models.length = 0;
+                }
+                var orderedModels = order || toAdd;
+                for (i = 0, length = orderedModels.length; i < length; i++) {
+                    this.models.push(orderedModels[i]);
+                }
+            }
+        }
+
+        // Sort the collection if appropriate.
+        if (sort) {
+            this.sort();
+        }
+
+        // Return the added (or merged) model (or models).
+        return singular ? models[0] : models;
     },
 
     // Remove a model, or a list of models from the set.
@@ -132,141 +256,6 @@ lodash.objects.assign(Collection.prototype, {
         return singular ? models[0] : models;
     },
 
-    // Update a collection by `set`-ing a new list of models, adding new ones,
-    // removing models that are no longer present, and merging models that
-    // already exist in the collection, as necessary. Similar to **Model#set**,
-    // the core operation for updating the data contained by the collection.
-    set: function (models, options) {
-        var defaultOptions,
-            singular,
-            id,
-            model,
-            attrs,
-            existing,
-            sort,
-            at,
-            sortable,
-            sortAttr,
-            toAdd,
-            toRemove,
-            modelMap,
-            add,
-            merge,
-            remove,
-            order,
-            orderChanged,
-            i;
-
-        defaultOptions = {add: true, remove: true, merge: true};
-        options = lodash.objects.defaults({}, options, defaultOptions);
-
-        // check if is a single model collection
-        singular = !lodash.objects.isArray(models);
-
-        models = singular ? (models ? [models] : []) : models.slice();
-        at = options.at;
-        if (at < 0) {
-            at += this.length + 1;
-        }
-        sortable = this.comparator && (at === null) && options.sort !== false;
-        sortAttr = lodash.objects.isString(this.comparator) ? this.comparator : null;
-
-        // initial setup of arrays and objects
-        toAdd = [];
-        toRemove = [];
-        modelMap = {};
-        add = options.add;
-        merge = options.merge;
-        remove = options.remove;
-        order = !sortable && add && remove ? [] : false;
-        orderChanged = false;
-
-        // Turn bare objects into model references, and prevent invalid models
-        // from being added.
-        for (i = 0, length = models.length; i < length; i++) {
-            attrs = models[i];
-
-            // If a duplicate is found, prevent it from being added and
-            // optionally merge it into the existing model.
-            existing = this.get(attrs);
-
-            if (existing) {
-                if (remove) {
-                    modelMap[existing.cid] = true;
-                }
-                if (merge && attrs !== existing) {
-                    attrs = this._isModel(attrs) ? attrs.attributes : attrs;
-                    existing.set(attrs, options);
-                    if (sortable && !sort && existing.hasChanged(sortAttr)) {
-                        sort = true;
-                    }
-                }
-                models[i] = existing;
-
-                // If this is a new, valid model, push it to the `toAdd` list.
-            } else if (add) {
-                model = models[i] = this._prepareModel(attrs, options);
-                if (!model) continue;
-                toAdd.push(model);
-                this._addReference(model, options);
-            }
-
-            // Do not add multiple models with the same `id`.
-            model = existing || model;
-            if (!model) continue;
-            id = this.modelId(model.attributes);
-            if (order && (model.isNew() || !modelMap[id])) {
-                order.push(model);
-                // Check to see if this is actually a new model at this index.
-                orderChanged = orderChanged || !this.models[i] || model.cid !== this.models[i].cid;
-            }
-
-            modelMap[id] = true;
-        }
-
-        // Remove nonexistent models if appropriate.
-        if (remove) {
-            for (i = 0, length = this.length; i < length; i++) {
-                if (!modelMap[(model = this.models[i]).cid]) {
-                    toRemove.push(model);
-                }
-            }
-            if (toRemove.length) {
-                this.remove(toRemove, options);
-            }
-        }
-
-        // See if sorting is needed, update `length` and splice in new models.
-        if (toAdd.length || orderChanged) {
-            if (sortable) {
-                sort = true;
-            }
-            this.length += toAdd.length;
-            if (at !== null) {
-                for (i = 0, length = toAdd.length; i < length; i++) {
-                    this.models.splice(at + i, 0, toAdd[i]);
-                }
-            } else {
-                if (order) {
-                    this.models.length = 0;
-                }
-                var orderedModels = order || toAdd;
-                for (i = 0, length = orderedModels.length; i < length; i++) {
-                    this.models.push(orderedModels[i]);
-                }
-            }
-        }
-
-        // Sort the collection if appropriate.
-        if (sort) {
-            this.sort();
-        }
-
-
-        // Return the added (or merged) model (or models).
-        return singular ? models[0] : models;
-    },
-
     // When you have more items than you want to add or remove individually,
     // you can reset the entire set with a new list of models
     // Useful for bulk operations and optimizations.
@@ -279,6 +268,100 @@ lodash.objects.assign(Collection.prototype, {
         this._reset();
         models = this.add(models, lodash.objects.assign({silent: true}, options));
         return models;
+    },
+
+    // Get a model from the set by id. Obj can either be the entire model attributes
+    // or just the id of the model
+    get: function (obj) {
+        var id;
+
+        // TODO create a better comparative to check if id is valid or not (maybe just if(id) ?)
+        if (obj == null) {
+            return void 0;
+        }
+
+        // if model get id from attributes, otherwise we already have the id
+        id = this.modelId(this._isModel(obj) ? obj.attributes : obj);
+
+        return this._byId[obj] || this._byId[id] || this._byId[obj.cid];
+    },
+
+    // Create a new collection with an identical list of models as this one.
+    clone: function () {
+        return new this.constructor(this.models, {
+            model: this.model,
+            comparator: this.comparator
+        });
+    },
+
+    // Define how to uniquely identify models in the collection.
+    modelId: function (attrs) {
+        return attrs[this.model.prototype.idAttribute || 'id'];
+    },
+
+    // Should be called by the model inside this collection in order to update
+    // the indexes, if necessary
+    modelUpdate: function (model) {
+        var prevId,
+            id;
+        prevId = this.modelId(model.previousAttributes());
+        id = this.modelId(model.attributes);
+        if (prevId !== id) {
+            // TODO create a better comparative to check if id is valid or not (maybe just if(id) ?)
+            if (prevId != null) delete this._byId[prevId];
+            if (id != null) this._byId[id] = model;
+        }
+    },
+
+    // Private method to reset all internal state. Called when the collection
+    // is first initialized or reset.
+    _reset: function () {
+        this.length = 0;
+        this.models = [];
+        this._byId = {};
+    },
+
+    // Prepare a hash of attributes (or other model) to be added to this
+    // collection.
+    _prepareModel: function (attrs, options) {
+        // if it is a model, save a reference of the collection and add it
+        if (this._isModel(attrs)) {
+          if (!attrs.collection) {
+              attrs.collection = this;
+          }
+          return attrs;
+        } else {
+            var model;
+            // if this is just a hash of attributes, create a new instance of the model
+            // of this collection
+            options = options ? lodash.objects.clone(options) : {};
+            options.collection = this;
+            return new this.model(attrs, options);
+        }
+    },
+
+    // Internal method to create a model's ties to a collection.
+    _addReference: function (model, options) {
+        var id;
+        this._byId[model.cid] = model;
+        id = this.modelId(model.attributes);
+        // TODO create a better comparative to check if id is valid or not (maybe just if(id) ?)
+        if (id != null) {
+            this._byId[id] = model;
+        }
+    },
+
+    // Internal method to sever a model's ties to a collection.
+    _removeReference: function (model, options) {
+        if (this === model.collection) {
+            delete model.collection;
+        }
+    },
+
+    // Method for checking whether an object should be considered a model for
+    // the purposes of adding to the collection.
+    _isModel: function (model) {
+        return model instanceof Model;
     },
 
     // Add a model to the end of the collection.
@@ -310,21 +393,6 @@ lodash.objects.assign(Collection.prototype, {
         return [].slice.apply(this.models, arguments);
     },
 
-    // Get a model from the set by id. Obj can either be the entire model attributes
-    // or just the id of the model
-    get: function (obj) {
-        var id;
-
-        if (obj === null) {
-            return void 0;
-        }
-
-        // if model get id from attributes, otherwise we already have the id
-        id = this.modelId(this._isModel(obj) ? obj.attributes : obj);
-
-        return this._byId[obj] || this._byId[id] || this._byId[obj.cid];
-    },
-
     // Get the model at the given index on the collection
     at: function (index) {
         if (index < 0) {
@@ -333,17 +401,17 @@ lodash.objects.assign(Collection.prototype, {
         return this.models[index];
     },
 
-    // Return models with matching attributes. Useful for simple cases of
-    // `filter`.
-    where: function (attrs, first) {
-        // detect which lodash method to use, find or filter based on first parameter
-        return this[first ? 'find' : 'filter'](this.models, attrs);
-    },
-
     // Return the first model with matching attributes. Useful for simple cases
     // of `find`.
     findWhere: function (attrs) {
         return this.where(attrs, true);
+    },
+
+    // Return models with matching attributes. Useful for simple cases of
+    // `filter`.
+    where: function (attrs, first) {
+        // detect which lodash method to use, find or filter based on first parameter
+        return lodash.collections[first ? 'find' : 'filter'](this.models, attrs);
     },
 
     // Force the collection to re-sort itself. You don't need to call this under
@@ -369,85 +437,15 @@ lodash.objects.assign(Collection.prototype, {
     // Pluck an attribute from each model in the collection.
     pluck: function (attr) {
         return lodash.collections.invoke(this.models, 'get', attr);
-    },
-
-    // Create a new collection with an identical list of models as this one.
-    clone: function () {
-        return new this.constructor(this.models, {
-            model: this.model,
-            comparator: this.comparator
-        });
-    },
-
-    // Define how to uniquely identify models in the collection.
-    modelId: function (attrs) {
-        return attrs[this.model.prototype.idAttribute || 'id'];
-    },
-
-    // Private method to reset all internal state. Called when the collection
-    // is first initialized or reset.
-    _reset: function () {
-        this.length = 0;
-        this.models = [];
-        this._byId  = {};
-    },
-
-    // Prepare a hash of attributes (or other model) to be added to this
-    // collection.
-    // TODO auto-mode for creating a model
-    _prepareModel: function (attrs, options) {
-        // if it is a model, save a reference of the collection and add it
-        if (this._isModel(attrs)) {
-          if (!attrs.collection) {
-              attrs.collection = this;
-          }
-          return attrs;
-        }
-        // if this is just a hash of attributes, create a new instance of the model
-        // of this collection
-        // TODO remove line bellow
-        options = options ? lodash.objects.clone(options) : {};
-        options.collection = this;
-        var model = new this.model(attrs, options);
-        if (model.isValid()) {
-            return model;
-        } else {
-            debug(model.validationError);
-            return false;
-        }
-    },
-
-    // Method for checking whether an object should be considered a model for
-    // the purposes of adding to the collection.
-    _isModel: function (model) {
-        return model instanceof Model;
-    },
-
-    // Internal method to create a model's ties to a collection.
-    _addReference: function (model, options) {
-        var id;
-        this._byId[model.cid] = model;
-        id = this.modelId(model.attributes);
-        if (id !== null) {
-            this._byId[id] = model;
-        }
-    },
-
-    // Internal method to sever a model's ties to a collection.
-    _removeReference: function (model, options) {
-        if (this === model.collection) {
-            delete model.collection;
-        }
-    },
-
+    }
 });
 
 // Lodash methods that we want to implement on the Collection.
 // 90% of the core usefulness of Onyx Collections is actually implemented here
 // all the aliases were removed (each -> forEach, and things like that)
-var methods = ['forEach', 'map', 'reduce', 'reduceRight', 'find', 'filter',
-    'reject', 'every', 'some', 'contains', 'invoke', 'max', 'min', 'toArray',
-    'size', 'first', 'last', 'shuffle', 'without', 'difference', 'initial', 'rest'];
+var methods = ['forEach', 'map', 'reduce', 'reduceRight', 'reject', 'every', 'some',
+    'contains', 'invoke', 'max', 'min', 'toArray', 'size', 'first', 'last', 'shuffle',
+    'without', 'difference', 'initial', 'rest'];
 
 // Mix in each Lodash method as a proxy to `Collection#models`.
 lodash.collections.forEach(methods, function (method) {
