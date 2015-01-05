0be5f2e1d0e4a5cde8d16d3db4eee7a9d89954e7
Fabio <fabiozaffani@gmail.com>
2015-01-05 19:03:37 -0200

    Added new dependency and extend update
    - Added dependency to the module backbone-extend-standalone
    - Changed the method Collection.extend to reference the module above

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ var lodash = {
         collections: require('lodash-node/modern/collections')
     },
     Model = require('model'),
+    extend = require('backbone-extend-standalone'),
     debug = function (message) { console.log(message); },
     Collection;
 
@@ -500,44 +501,6 @@ lodash.collections.forEach(attributeMethods, function (method) {
 });
 
 // same extend function used by backbone
-Collection.extend = function (protoProps, staticProps) {
-    var parent,
-        child,
-        Surrogate;
-
-    parent = this;
-
-    // The constructor function for the new subclass is either defined by you
-    // (the "constructor" property in your `extend` definition), or defaulted
-    // by us to simply call the parent's constructor.
-    if (protoProps && lodash.objects.has(protoProps, 'constructor')) {
-        child = protoProps.constructor;
-    } else {
-        child = function () {
-            return parent.apply(this, arguments);
-        };
-    }
-
-    // Add static properties to the constructor function, if supplied.
-    lodash.objects.assign(child, parent, staticProps);
-
-    // Set the prototype chain to inherit from `parent`, without calling
-    // `parent`'s constructor function.
-    Surrogate = function () {
-        this.constructor = child;
-    };
-    Surrogate.prototype = parent.prototype;
-    child.prototype = new Surrogate;
-
-    // Add prototype properties (instance properties) to the subclass,
-    // if supplied.
-    if (protoProps) lodash.objects.assign(child.prototype, protoProps);
-
-    // Set a convenience property in case the parent's prototype is needed
-    // later.
-    child.__super__ = parent.prototype;
-
-    return child;
-};
+Collection.extend = extend;
 
 module.exports = Collection;
